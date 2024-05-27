e9d23218d2c542af4596391d1a10894c4d99500a
Patimat <153724188+DrPatysik@users.noreply.github.com>
2024-05-27 17:59:56 +0300

    + assets (font)

--- app/src/main/java/com/example/myspinner/MainActivity.kt
+++ app/src/main/java/com/example/myspinner/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myspinner
 
+import android.graphics.Typeface
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Spinner
@@ -14,14 +15,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        spinner = findViewById(R.id.spinner)
-        textField = findViewById(R.id.text1)
-
-        val button1: Button = findViewById(R.id.button2)
-        button1.setOnClickListener {
-            val spinnerPosition = spinner?.selectedItemPosition
-            textField?.text = showDescription(spinnerPosition!!)
-        }
+        init()
     }
 
     private fun showDescription(index: Int): String {
@@ -29,4 +23,20 @@ class MainActivity : AppCompatActivity() {
         return descriptionArray[index]
     }
 
+    private fun init(){
+        spinner = findViewById(R.id.spinner)
+        textField = findViewById(R.id.text1)
+
+        val button1: Button = findViewById(R.id.button2)
+        val tf2 = Typeface.createFromAsset(assets,"bubblez_graffitiItalic.ttf")
+        button1.setTypeface(tf2)
+
+        button1.setOnClickListener {
+            val spinnerPosition = spinner?.selectedItemPosition
+
+            val tf = Typeface.createFromAsset(assets,"ofont.ru_brushboy.ttf")
+            textField?.setTypeface(tf)
+            textField?.text = showDescription(spinnerPosition!!)
+        }
+    }
 }
